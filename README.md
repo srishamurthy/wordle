cf6da132e9c2ba0e56f0d3d7dcc3824e20804254
Srisha Murthy <nsrishamurthy711@gmail.com>
2024-06-25 00:23:41 -0400

    initial outline

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wordle
+A Wordle solver algorithm that uses information theory, as described by 3b1b.
---
+++ solver.py
@@ -0,0 +1,7 @@
+# read words from dataset
+
+# generate color patterns for words
+
+# frequency of words, sigmoid fct
+
+# entropy computations
